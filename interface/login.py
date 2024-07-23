b8e581c768d7600452ea8365f4248b1522c66b61
Douglas Freitas <douglasamf14@gmail.com>
2024-07-22 22:46:37 -0300

    adding assets

---
+++ interface/login.py
@@ -0,0 +1,8 @@
+import flet as ft
+
+def main(page: ft.Page):
+
+    ...
+
+if __name__=='__main__':
+    ft.app(target=main)
---
+++ interface/main.py
@@ -0,0 +1,8 @@
+import flet as ft
+
+def main(page: ft.Page):
+
+    page.title = 'Kingdoms of Valeria'
+
+if __name__=='__main__':
+    ft.app(target=main)
